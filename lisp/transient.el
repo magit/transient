846e0c200ce178026adcf6000a0725779261beba
Jonas Bernoulli <jonas@bernoul.li>
2020-01-02 23:04:54 +0100

    Update copyright years

--- lisp/transient.el
+++ lisp/transient.el
@@ -1,6 +1,6 @@
 ;;; transient.el --- Transient commands          -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2018-2019  Jonas Bernoulli
+;; Copyright (C) 2018-2020  Jonas Bernoulli
 
 ;; Author: Jonas Bernoulli <jonas@bernoul.li>
 ;; Homepage: https://github.com/magit/transient
