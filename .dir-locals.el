902121268d72045da53c711932ff72017a33ce6c
Jonas Bernoulli <jonas@bernoul.li>
2025-05-09 16:40:32 +0200

    Add .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,9 @@
+((nil
+  (indent-tabs-mode . nil))
+ (makefile-mode
+  (indent-tabs-mode . t))
+ (git-commit-mode
+  (git-commit-major-mode . git-commit-elisp-text-mode))
+ ("CHANGELOG"
+  (nil (fill-column . 70)
+       (mode . display-fill-column-indicator))))
