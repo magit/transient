aa32e0d66cc389befed7a8e8df9439d92a729daa
Jonas Bernoulli <jonas@bernoul.li>
2025-08-01 13:49:42 +0200

    Release version 0.9.4

--- lisp/transient.el
+++ lisp/transient.el
@@ -6,7 +6,7 @@
 ;; Homepage: https://github.com/magit/transient
 ;; Keywords: extensions
 
-;; Package-Version: 0.9.3
+;; Package-Version: 0.9.4
 ;; Package-Requires: ((emacs "26.1") (compat "30.1") (seq "2.24"))
 
 ;; SPDX-License-Identifier: GPL-3.0-or-later
@@ -34,7 +34,7 @@
 
 ;;; Code:
 
-(defconst transient-version "0.9.3")
+(defconst transient-version "0.9.4")
 
 (require 'cl-lib)
 (require 'compat)
