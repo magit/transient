f40a503dcb4077bf1ea0f3268e7f16e1142d2ec4
Jonas Bernoulli <jonas@bernoul.li>
2026-01-04 17:50:30 +0100

    [patch 5/10] lisp: Stop using when$ and and$
    That's fair.

--- lisp/transient.el
+++ lisp/transient.el
@@ -1309,8 +1309,8 @@ commands are aliases for."
   (cl-typecase spec
     (null    (error "Invalid transient--parse-child spec: %s" spec))
     (symbol  (list `',spec))
-    (vector  (and$ (transient--parse-group  prefix spec) (list $)))
-    (list    (and$ (transient--parse-suffix prefix spec) (list $)))
+    (vector  (and-let* ((c (transient--parse-group  prefix spec))) (list c)))
+    (list    (and-let* ((c (transient--parse-suffix prefix spec))) (list c)))
     (string  (list spec))
     (t       (error "Invalid transient--parse-child spec: %s" spec))))
 
@@ -2278,9 +2278,9 @@ of the corresponding object."
                  (error "Cannot bind %S to %s and also %s"
                         (string-trim key) cmd alt))
                 ((define-key map kbd cmd))))))
-    (when$ (keymap-lookup map "-") (keymap-set map "<kp-subtract>" $))
-    (when$ (keymap-lookup map "=") (keymap-set map "<kp-equal>" $))
-    (when$ (keymap-lookup map "+") (keymap-set map "<kp-add>" $))
+    (when-let* ((b (keymap-lookup map "-"))) (keymap-set map "<kp-subtract>" b))
+    (when-let* ((b (keymap-lookup map "="))) (keymap-set map "<kp-equal>" b))
+    (when-let* ((b (keymap-lookup map "+"))) (keymap-set map "<kp-add>" b))
     (when transient-enable-popup-navigation
       ;; `transient--make-redisplay-map' maps only over bindings that are
       ;; directly in the base keymap, so that cannot be a composed keymap.
@@ -2607,8 +2607,8 @@ value.  Otherwise return CHILDREN as is.")
      t)))
 
 (defun transient--inapt-suffix-p (obj)
-  (or (and$ (oref obj parent)
-            (oref $ inapt))
+  (or (and-let* ((parent (oref obj parent)))
+        (oref parent inapt))
       (let ((transient--shadowed-buffer (current-buffer))
             (transient--pending-suffix obj))
         (transient--do-suffix-p
@@ -2649,8 +2649,8 @@ value.  Otherwise return CHILDREN as is.")
 (defun transient--suffix-predicate (spec)
   (let ((props (transient--suffix-props spec)))
     (seq-some (lambda (prop)
-                (and$ (plist-get props prop)
-                      (list prop $)))
+                (and-let* ((pred (plist-get props prop)))
+                  (list prop pred)))
               '( :if :if-not
                  :if-nil :if-non-nil
                  :if-mode :if-not-mode
@@ -5235,8 +5235,8 @@ See `forward-button' for information about N."
        (goto-char (match-beginning 0))))
     (command
      (cl-flet ((found ()
-                 (and$ (button-at (point))
-                       (eq (button-get $ 'command) command))))
+                 (and-let* ((button (button-at (point))))
+                   (eq (button-get button 'command) command))))
        (while (and (ignore-errors (forward-button 1))
                    (not (found))))
        (unless (found)
@@ -5466,8 +5466,8 @@ as stand-in for elements of exhausted lists."
 
 (cl-defmethod transient-infix-value ((obj transient-cons-option))
   "Return ARGUMENT and VALUE as a cons-cell or nil if the latter is nil."
-  (and$ (oref obj value)
-        (cons (oref obj argument) $)))
+  (and-let* ((value (oref obj value)))
+    (cons (oref obj argument) value)))
 
 (cl-defmethod transient-format-description ((obj transient-cons-option))
   (or (oref obj description)
@@ -5490,10 +5490,8 @@ as stand-in for elements of exhausted lists."
 ;;   (cond . 0)
 ;;   (interactive . 0))
 ;; read-symbol-shorthands: (
-;;   ("and$"         . "cond-let--and$")
 ;;   ("and-let"      . "cond-let--and-let")
 ;;   ("if-let"       . "cond-let--if-let")
-;;   ("when$"        . "cond-let--when$")
 ;;   ("when-let"     . "cond-let--when-let")
 ;;   ("while-let"    . "cond-let--while-let"))
 ;; End:
