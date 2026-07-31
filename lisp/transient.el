9ba468bcbc7f6061c02a5319ff961f3e5ba6fa13
Jonas Bernoulli <jonas@bernoul.li>
2026-08-01 00:43:23 +0200

    Release version 0.13.6

--- lisp/transient.el
+++ lisp/transient.el
@@ -6,7 +6,7 @@
 ;; Homepage: https://github.com/magit/transient
 ;; Keywords: extensions
 
-;; Package-Version: 0.13.5
+;; Package-Version: 0.13.6
 ;; Package-Requires: (
 ;;     (emacs   "28.1")
 ;;     (compat  "31.0")
@@ -45,7 +45,7 @@
 
 ;;; Code:
 
-(defconst transient-version "0.13.5")
+(defconst transient-version "0.13.6")
 
 (require 'cl-lib)
 (require 'compat)
