416bddb37eda2f838f1e08dddde208dae271cc39
Jonas Bernoulli <jonas@bernoul.li>
2026-08-15 21:07:35 +0200

    .dir-locals.el: Set lisp-indent-local-overrides

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,14 @@
 ((nil
-  (indent-tabs-mode . nil)
   (copyright-names-regexp
-   . "\\(?:Jonas Bernoulli\\|Free Software Foundation, Inc\\.\\)"))
+   . "\\(?:Jonas Bernoulli\\|Free Software Foundation, Inc\\.\\)")
+  (indent-tabs-mode . nil)
+  (lisp-indent-local-overrides
+   (cond . 0)
+   (cond-let--thread$ . defun)
+   (interactive . 0)
+   (make-obsolete-variable . 1)
+   (thread-first . defun)
+   (thread-last . defun)))
  (makefile-mode
   (indent-tabs-mode . t))
  (git-commit-mode
